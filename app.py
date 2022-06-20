6ae41b63044e466a104dc99c019334f2cd6444f8
mgurga <motiurga@gmail.com>
2022-06-20 00:15:25 -0700

    inital commit

---
+++ app.py
@@ -0,0 +1,134 @@
+from flask import Flask, send_from_directory, request
+from apscheduler.schedulers.background import BackgroundScheduler
+from os.path import join, dirname
+from copy import copy
+from datetime import date, datetime
+
+app = Flask("multiplayer conways game of life")
+public = join(dirname(__file__), "public")
+
+UPDATE_INTERVAL = 5
+class Cell:
+    def __init__(self, x: int, y: int, r: int, g: int, b: int, alive: bool = True) -> None:
+        (self.x, self.y, self.r, self.g, self.b, self.alive) = x, y, r, g, b, alive
+    def __str__(self) -> str:
+        return f"x: {self.x} y: {self.y} alive: {self.alive}"
+    def to_dict(self) -> dict:
+        return {"x": self.x, "y": self.y, "r": self.r, "g": self.g, "b": self.b}
+cells: set[Cell] = set()
+
+@app.route("/")
+def index(): return send_from_directory(public, "index.html")
+@app.route("/play")
+def play(): return send_from_directory(public, "play.html")
+@app.route("/game.js")
+def js(): return send_from_directory(public, "game.js")
+
+@app.route("/api/addcell", methods=["POST"])
+def add_cell():
+    req = request.get_json()
+    if req != None:
+        print(f"adding cell at x: {req['x']}, y: {req['y']}")
+        nc = Cell(req['x'], req['y'], req['r'], req['g'], req['b'], True)
+        for c in cells:
+            if c.x == nc.x and c.y == nc.y:
+                cells.remove(c)
+                break
+        cells.add(nc)
+        add_dead_cells_around(Cell(req['x'], req['y'], req['r'], req['g'], req['b']))
+        return "OK", 200
+    else:
+        return "ERROR", 404
+
+@app.route("/api/getcells")
+def get_cells():
+    global cells
+    out: dict = {}
+    clist = list(cells)
+    for i in range(0, len(clist)):
+        if clist[i].alive:
+            out = out | {i: clist[i].to_dict()}
+    return out
+
+def step():
+    stepstart = datetime.now()
+    global cells
+    newcells: set[Cell] = set()
+    newcpos: list[Cell] = []
+
+    for c in cells:
+        adj = neighbors(c)
+        newc = copy(c)
+
+        # 1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+        if adj == 1 or adj == 0 and c.alive:
+            newc.alive = False
+            newcells.add(newc)
+
+        # 2. Any live cell with two or three live neighbours lives on to the next generation.
+        if (adj == 2 or adj == 3) and c.alive:
+            newcells.add(newc)
+
+        # 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+        if adj > 3 and c.alive:
+            newc.alive = False
+            newcells.add(newc)
+
+        # 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+        if adj == 3 and c.alive == False:
+            newc.alive = True
+            newcells.add(newc)
+            newcpos.append(newc)
+        
+        # carry over dead cells
+        if c.alive == False:
+            newcells.add(newc)
+
+    cells = newcells
+    for new_cell in newcpos:
+        add_dead_cells_around(new_cell)
+    print(f"step finished. took {datetime.now() - stepstart}")
+
+def add_dead_cells_around(c: Cell):
+    for i in range(-1, 2):
+        if not cell_at(c.x + i, c.y + 1):
+            cells.add(Cell(c.x + i, c.y + 1, c.r, c.g, c.b, False))
+        if not cell_at(c.x + i, c.y - 1):
+            cells.add(Cell(c.x + i, c.y - 1, c.r, c.g, c.b, False))
+
+    if not cell_at(c.x + 1, c.y):
+        cells.add(Cell(c.x + 1, c.y, c.r, c.g, c.b, False))
+    if not cell_at(c.x - 1, c.y):
+        cells.add(Cell(c.x - 1, c.y, c.r, c.g, c.b, False))
+
+def neighbors(cell: Cell) -> int:
+    out = 0
+    for c in cells:
+        if c.alive:
+            if cell.x == c.x + 1 and cell.y == c.y - 1: # upper left
+                out = out + 1
+            if cell.x == c.x and cell.y == c.y - 1: # upper middle
+                out = out + 1
+            if cell.x == c.x - 1 and cell.y == c.y - 1: # upper right
+                out = out + 1
+            if cell.x == c.x + 1 and cell.y == c.y: # middle left
+                out = out + 1
+            if cell.x == c.x - 1 and cell.y == c.y: # middle right
+                out = out + 1
+            if cell.x == c.x + 1 and cell.y == c.y + 1: # lower left
+                out = out + 1
+            if cell.x == c.x and cell.y == c.y + 1: # lower middle
+                out = out + 1
+            if cell.x == c.x - 1 and cell.y == c.y + 1: # lower right
+                out = out + 1
+    return out
+
+def cell_at(x: int, y: int) -> bool:
+    for c in cells:
+        if c.x == x and c.y == y:
+            return True
+    return False
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(step, 'interval', seconds=UPDATE_INTERVAL)
+sched.start()
---
+++ public/game.js
@@ -0,0 +1,113 @@
+var c = document.getElementsByTagName('canvas')[0];
+var ctx = c.getContext('2d');
+var deviceWidth = window.innerWidth;
+var deviceHeight = window.innerHeight;
+var nativeWidth = 1600;
+var nativeHeight = 900;
+var scaleFillNative = Math.max(deviceWidth / nativeWidth, deviceHeight / nativeHeight);
+c.width = deviceWidth;
+c.height = deviceHeight;
+
+var squaresize = 40
+var boardsize = 401
+var scrollx = 0
+var scrolly = 0
+var usercolor = [Math.floor(rand(150, 255)), Math.floor(rand(150, 255)), Math.floor(rand(150, 255))]
+var dragging = false
+var dragstart = [0, 0]
+var dragpos = [0, 0]
+var mousepos = [0, 0]
+var realscroll = [0, 0]
+var cells = []
+
+function draw() {
+    drawGrid()
+    drawCells()
+    if (dragging) {
+        scrollx += mousepos[0] - dragpos[0]
+        scrolly += mousepos[1] - dragpos[1]
+        realscroll[0] += mousepos[0] - dragpos[0]
+        realscroll[1] += mousepos[1] - dragpos[1]
+        dragpos[0] = mousepos[0]
+        dragpos[1] = mousepos[1]
+    }
+    requestAnimationFrame(draw);
+}
+
+function drawCells() {
+    for (const c of cells) {
+        ctx.fillStyle = "rgb(" + c.r + "," + c.g + "," + c.b + ")"
+        ctx.fillRect((c.x * squaresize) + realscroll[0], (c.y * squaresize) + realscroll[1], squaresize, squaresize)
+        // ctx.fillStyle = "rgb(0, 0, 0)"
+        // ctx.fillText("x: " + c.x, (c.x * squaresize) + realscroll[0], (c.y * squaresize) + realscroll[1] + 10)
+        // ctx.fillText("y: " + c.y, (c.x * squaresize) + realscroll[0], (c.y * squaresize) + realscroll[1] + 20)
+    }
+}
+
+function drawGrid() {
+    ctx.fillStyle = "#000000"
+    ctx.fillRect(0, 0, c.width, c.height)
+    ctx.strokeStyle = "#111111"
+
+    for (var x = -boardsize; x < boardsize; x++) {
+        for (var y = -boardsize; y < boardsize; y++) {
+            if (x * squaresize + scrollx < c.width && x * squaresize + scrollx > -squaresize) { // check x if rendered
+                if (y * squaresize + scrolly < c.height && y * squaresize + scrolly > -squaresize) { // check y if rendered
+                    ctx.strokeRect((x * squaresize) + scrollx, (y * squaresize) + scrolly, squaresize, squaresize)
+                }
+            }
+        }
+    }
+
+    if (scrollx > squaresize) { scrollx = scrollx - squaresize }
+    if (scrolly > squaresize) { scrolly = scrolly - squaresize }
+    if (scrollx < -squaresize) { scrollx = scrollx + squaresize }
+    if (scrolly < -squaresize) { scrolly = scrolly + squaresize }
+}
+
+function mouseClick(e) {
+    var blockx = Math.floor((e.clientX - realscroll[0]) / squaresize)
+    var blocky = Math.floor((e.clientY - realscroll[1]) / squaresize)
+    console.log("clicked x: " + blockx + " y: " + blocky);
+    addCell(blockx, blocky)
+}
+
+function addCell(x, y) {
+    var data = {"x": x, "y": y, "r": usercolor[0], "g": usercolor[1], "b": usercolor[2]}
+    fetch("/api/addcell", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify(data)
+    }).then(res => {
+        getCells()
+    });
+}
+
+function getCells() {
+    fetch("/api/getcells")
+    .then(res => res.json())
+    .then(data => {
+        cells = []
+        for (var c in data) {
+            cells.push(data[c])
+        }
+    })
+}
+
+function rand(min, max) { return Math.random() * (max - min) + min; }
+c.addEventListener("mousedown", (e) => {
+    dragging = true
+    dragpos[0] = dragstart[0] = e.clientX
+    dragpos[1] = dragstart[1] = e.clientY
+});
+c.addEventListener("mousemove", (e) => {
+    [mousepos[0], mousepos[1]] = [e.clientX, e.clientY]
+});
+c.addEventListener("mouseup", (e) => {
+    dragging = false
+    if (dragstart[0] == mousepos[0] && dragstart[1] == mousepos[1]) { mouseClick(e) }
+});
+
+draw()
+var getCellInt = setInterval(getCells, 1000)
+getCells()
